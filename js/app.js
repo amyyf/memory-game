47af0ad192a37b729a9ad15c6cf4042cf85490a5
Amy Frieson <ava447@gmail.com>
2018-06-02 17:57:01 -0400

    refactor: Remove unnec 'else' statement from flip function

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ container.addEventListener('click', function (event) {
   let card = event.target;
   if (card.classList.contains('flip')) {
     return;
-  } else {
-    card.classList.toggle('flip');
   }
+  card.classList.toggle('flip');
 });
